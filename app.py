48f09d49cb8666f263569ce05e3efcd7638903c7
Dhilip1997 <dhilipvasanth@gmail.com>
2022-09-27 22:51:18 +0530

    Connected with MongoDb

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 from flask import Flask, render_template, request
 import datetime
 from pymongo import MongoClient
+import webbrowser
+from threading import Timer
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(
+        "mongodb+srv://dhilipmaharish:dibots97@microblog.chrtjkp.mongodb.net/test")
+    app.db = client.micro_blog
 
-# client = MongoClient("mongodb+srv://dhilip1997:dibots97@blogging.sgsphrd.mongodb.net/?retryWrites=true&w=majority")
-# entries =[]
-# app.db = client.blogs
+    @app.route("/", methods=["GET", "POST"])
+    def display():
+        if request.method == "POST":
+            entry_content = request.form.get("blog_story")
+            formated_date = datetime.datetime.today().strftime("%y-%m-%d")
+            #entries.append((entry_content, formated_date))
+            # print(entries)
+            app.db.entities.insert_one(
+                {"content": entry_content, "date": formated_date})
+            for ele in app.db.entities.find({}):
+                print(ele)
 
-@app.route("/", methods=["GET","POST"])
-def display():
-    if request.method == "POST":
-        entry_content = request.form.get("blog_story")
-        formated_date = datetime.datetime.today().strftime("%y-%m-%d")
-    #     entries.append((entry_content, formated_date))
-    #     app.db.entries.insert_one({"content": entry_content, "date": formated_date})
+        entry_with_date = [(entry["content"], entry["date"], datetime.datetime.strptime(
+            entry["date"], "%y-%m-%d").strftime("%b %d '%y")) for entry in app.db.entities.find({})]
+        print(entry_with_date)
+        return render_template("index.html", entries = entry_with_date)
 
-    # entry_with_date = [(entry["content"], entry["date"], datetime.datetime.strptime(entry["date"], "%y-%m-%d").strftime("%b %d '%y")) for entry in app.db.entries.find({})]
-    return render_template("index.html")
-    
-app.run(port= 8080)
+    return app
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
+create_app().run(port=5000)
+
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+    create_app.run(port=5000)
